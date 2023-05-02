4f11bfa5e74f919842397ba78eff41146abcea93
flandrecorentin <flandrecorentin@gmail.com>
2023-05-02 14:17:30 +0200

    Routerpage component

---
+++ h4221-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
+import {Input, NgModule} from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  // { path: 'curriculum', component: CurriculumComponent},
+  // { path: 'projects', component: ProjectsComponent},
+  // { path: 'hobbies/sport', component: HobbiesSportComponent},
+  // { path: 'hobbies/art', component: HobbiesArtComponent},
+  // { path: 'hobbies', component: HobbiesComponent},
+  // { path: 'about', component: AboutComponent},
+  // { path: 'contact', component: ContactComponent},
+  // { path: 'svg/:id', component: SvgComponent},
+  // { path: 'svg/:id', component: PageNotFoundComponent},
+  // { path: 'index.html',   redirectTo: '', pathMatch: 'full' },
+  // Route for 404 request : resources not found
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- h4221-frontend/src/app/app.module.ts
+++ h4221-frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
