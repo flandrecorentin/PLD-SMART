b824c20a2bae581a425604f5f79e3c1e16c1e452
flandrecorentin <flandrecorentin@gmail.com>
2023-05-02 12:13:35 +0200

    Initialisation

---
+++ h4221-backend/src/main/java/ifinsa/h4221backend/H4221BackendApplication.java
@@ -0,0 +1,13 @@
+package ifinsa.h4221backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H4221BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H4221BackendApplication.class, args);
+    }
+
+}
---
+++ h4221-backend/src/test/java/ifinsa/h4221backend/H4221BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ifinsa.h4221backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H4221BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
