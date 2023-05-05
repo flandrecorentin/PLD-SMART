54cba8d197b192d1c4809b652598352174186ffe
ThomasColin1 <saumontomate@gmail.com>
2023-05-05 10:48:17 +0200

    Premiers essais connection avec le back

---
+++ h4221-frontend/src/app/config/config.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+}
