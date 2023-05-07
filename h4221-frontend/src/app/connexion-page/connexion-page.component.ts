0bb8eeb6ca4e6dbdf4cb9e77dff9e2b418867e12
LAPTOP-F4TOHIAV\Tom <tom.delaporte@insa-lyon.fr>
2023-05-07 19:18:34 +0200

    FEAT - Form handling and first draft complete form

--- h4221-frontend/src/app/connexion-page/connexion-page.component.ts
+++ h4221-frontend/src/app/connexion-page/connexion-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-connexion-page',
