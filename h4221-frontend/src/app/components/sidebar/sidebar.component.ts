16d4e885d44b67449f7a072a3c961df7ada5341d
ThomasColin1 <saumontomate@gmail.com>
2023-05-07 12:53:24 +0200

    Sidebar !!! (pas responsive cela dit)

--- h4221-frontend/src/app/components/sidebar/sidebar.component.ts
+++ h4221-frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { navbarData } from './nav-data';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,6 +9,15 @@ import { navbarData } from './nav-data';
 })
 export class SidebarComponent {
   navData = navbarData;
+  constructor(private fb: FormBuilder) { }
 
+  rechercheForm = this.fb.group({
+    recherche: ["", Validators.required]
+  })
+
+  submitRecherche() {
+    //Here, we will call the service in order to log the user to Mobilit'if
+    console.log(this.rechercheForm.value);
+  }
 
 }
