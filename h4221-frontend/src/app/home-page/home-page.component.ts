ba468ac4269cd645bfe035426b3463e753d9dce4
ThomasColin1 <saumontomate@gmail.com>
2023-05-09 14:53:27 +0200

    Map un peu avancée

--- h4221-frontend/src/app/home-page/home-page.component.ts
+++ h4221-frontend/src/app/home-page/home-page.component.ts
@@ -15,6 +15,26 @@ export class HomePageComponent {
   ngOnInit() {
   }
   
+  mockUniversites=[
+    {name:"GE3 - Institute of International Education (IIE)", site:"google.com", fiche:"google.com"},
+    {name:"Illinois Institute of Technology", site:"google.com", fiche:"google.com"},
+    {name:"Georgia Institute of Technology (Georgia Tech)", site:"google.com", fiche:"google.com"},
+    {name:"International Student Exchange Program (ISEP)", site:"google.com", fiche:"google.com"},
+    {name:"GE3 - Institute of International Education (IIE)", site:"google.com", fiche:"google.com"},
+    {name:"Illinois Institute of Technology", site:"google.com", fiche:"google.com"},
+    {name:"Georgia Institute of Technology (Georgia Tech)", site:"google.com", fiche:"google.com"},
+    {name:"International Student Exchange Program (ISEP)", site:"google.com", fiche:"google.com"},
+  ]
+  univs=this.mockUniversites;
+
+  mockQas=[
+    {question:"Is there a free trial available?", answer:"Yes, you can try us for free for 30 days. Our friendly team will work with you to get you up and running as soon as possible.", type:"logement"},
+    {question:"Is there a free trial available?", answer:"Yes, you can try us for free for 30 days. Our friendly team will work with you to get you up and running as soon as possible.", type:"logement"},
+    {question:"Is there a free trial available?", answer:"Yes, you can try us for free for 30 days. Our friendly team will work with you to get you up and running as soon as possible.", type:"logement"},
+    {question:"Is there a free trial available?", answer:"Yes, you can try us for free for 30 days. Our friendly team will work with you to get you up and running as soon as possible.", type:"logement"},
+    {question:"Is there a free trial available?", answer:"Yes, you can try us for free for 30 days. Our friendly team will work with you to get you up and running as soon as possible.", type:"logement"},
+  ]
+  qas=this.mockQas;
 
 }
 
