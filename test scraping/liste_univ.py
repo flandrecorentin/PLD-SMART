4a1633dc564133e1ad0056919c069b2d81b6f5e1
dorian-brd <bernard.dorian1@gmail.com>
2023-05-09 22:25:34 +0200

    script complet

--- test scraping/liste_univ.py
+++ test scraping/liste_univ.py
@@ -13,7 +13,7 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 url = 'https://insalyon.adv-pub.moveonfr.com/report-page-1653/'
 driver.maximize_window() #maximize the window
 driver.get(url)          #open the URL
-driver.implicitly_wait(220) #maximum time to load the link
+driver.implicitly_wait(5) #maximum time to load the link
 
 #initialisation donnes
 etablissement = {}
@@ -24,63 +24,69 @@ for i in range (1,2) :
     time.sleep(2)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight,)")
 
-plusInfo = driver.find_elements(By.LINK_TEXT, "Afficher plus")
+afficherPlus = driver.find_element(By.LINK_TEXT, "Afficher plus")
 time.sleep(1)
-for plus in plusInfo:
+driver.execute_script("window.scrollTo(0, 0)")
+time.sleep(3)
+try:
+    while 1:
+        afficherPlus = driver.find_element(By.LINK_TEXT, "Afficher plus")
+        afficherPlus.location_once_scrolled_into_view
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, window.scrollY - 200)")
+        time.sleep(0.2)
+        afficherPlus.click()
+except:
+    #Ouverture de la page "Plus d'infos"
+    print("coucou")
+    listenom = driver.find_elements(By.XPATH, '//div[@class="_univname"]')
+    listepays = driver.find_elements(By.XPATH, '//div[@class ="university_stats"]/p[text() = "Pays"]/../../following-sibling::div/div/p')
+    listeAccord = driver.find_elements(By.XPATH, '//div[@class ="university_stats"]/p[text() = "Accord"]/../../following-sibling::div/div/p')
+    i =0
+    time.sleep(2)
 
-    plusInfo.location_once_scrolled_into_view
-    time.sleep(1)
-    driver.execute_script("window.scrollTo(0, window.scrollY - 200)")
-    time.sleep(1)
-#Ouverture de la page "Plus d'infos"
-plusInfo.click()
+    """
+    for t in listePlus :
+        time.sleep(1)
+        print(t.text)
+        driver.execute_script("arguments[0].scrollIntoView();",t)
+    time.sleep(2)
+    """
 
-listePlus = driver.find_elements(By.LINK_TEXT, "Plus de détails")
-listenom = driver.find_elements(By.XPATH, '//div[@class="_univname"]')
-listepays = driver.find_elements(By.XPATH, '//div[@class ="university_stats"]/p[text() = "Pays"]/../../following-sibling::div/div/p')
-listeAccord = driver.find_elements(By.XPATH, '//div[@class ="university_stats"]/p[text() = "Accord"]/../../following-sibling::div/div/p')
-i =0
-time.sleep(2)
-for t in listePlus :
-    time.sleep(1)
-    print(t.text)
-    driver.execute_script("arguments[0].scrollIntoView();",t)
-time.sleep(2)
+    """
+    for lien in listePlus :
+        driver.execute_script("arguments[0].scrollIntoView();", lien)
+        lien.click()
+    """
 
+    for nomuniversite in listenom:
+        print("--------------------------")
+        print(listepays[i].text)
+        nomuniversite = nomuniversite.text
+        #Normalisation des noms d'université
+        idNom = nomuniversite.lower()
+        idnom = idNom.replace(" ","-")
+        idnom = unidecode.unidecode(idnom)
+        print(nomuniversite)
+        print(idnom)
+        if idnom not in listeEtablissement : 
+            etablissement = {}
+            Accord = []
+            Accord.append(unidecode.unidecode(listeAccord[i].text))
+            etablissement["nom"]=unidecode.unidecode(nomuniversite)
+            etablissement["pays"]=unidecode.unidecode(listepays[i].text)
+            etablissement["accord"]=Accord
+            listeEtablissement[idnom] = etablissement
+        else:
+            listeEtablissement[idnom]['accord'].append(unidecode.unidecode(listeAccord[i].text))
+        i = i+1
 
+        #a = driver.find_element(By.CLASS_NAME, "university_stats")
 
-for lien in listePlus :
-    driver.execute_script("arguments[0].scrollIntoView();", lien)
-    lien.click()
-
-for nomuniversite in listenom:
-    print("--------------------------")
-    print(listepays[i].text)
-    nomuniversite = nomuniversite.text
-    #Normalisation des noms d'université
-    idNom = nomuniversite.lower()
-    idnom = idNom.replace(" ","-")
-    idnom = unidecode.unidecode(idnom)
-    print(nomuniversite)
-    print(idnom)
-    if idnom not in listeEtablissement : 
-        etablissement = {}
-        Accord = []
-        Accord.append(listeAccord[i].text)
-        etablissement["nom"]=nomuniversite
-        etablissement["pays"]=listepays[i].text
-        etablissement["accord"]=Accord
-        listeEtablissement[idnom] = etablissement
-    else:
-        listeEtablissement[idnom]['accord'].append(listeAccord[i].text)
-    i = i+1
-
-    #a = driver.find_element(By.CLASS_NAME, "university_stats")
-
-print(listeEtablissement)
-with open('liste_univ_pays_accord.json', 'w') as mon_fichier:
-    json.dump(listeEtablissement, mon_fichier)
-driver.close()
+    print(listeEtablissement)
+    with open('liste_univ_pays_accord.json', 'w') as mon_fichier:
+        json.dump(listeEtablissement, mon_fichier)
+    driver.close()
 
 
 
--- test scraping/test.py
+++ test scraping/test.py
@@ -16,7 +16,7 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=chrome_opt
 url = 'https://insalyon.adv-pub.moveonfr.com/report-page-1653/'
 driver.maximize_window() #maximize the window
 driver.get(url)          #open the URL
-driver.implicitly_wait(220) #maximum time to load the link
+driver.implicitly_wait(22) #maximum time to load the link
 
 #initialisation donnees
 etablissement = {}
@@ -51,56 +51,164 @@ time.sleep(2)
 """
 
 #Scrolling parfait pour ouvrir la première page "Plus d'infos"
-plusInfo = driver.find_element(By.LINK_TEXT, "Plus de détails")
-time.sleep(1)
-plusInfo.location_once_scrolled_into_view
-time.sleep(1)
-driver.execute_script("window.scrollTo(0, window.scrollY - 200)")
-time.sleep(1)
-#Ouverture de la page "Plus d'infos"
-plusInfo.click()
-time.sleep(2)
-#Ouverture de l'onglet "etablissement"
-univBtn = driver.find_element(By.LINK_TEXT, "Etablissements")
-univBtn.click()
-time.sleep(2)
-#Fermeture de la page "Plus d'infos"
-closeBtn = driver.find_element(By.XPATH, '//button[@class="_modal_univ_moreclose"]')
-closeBtn.click()
 
-
-"""
-wait = WebDriverWait(driver, 10)
-element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Plus de détails")))
-print(element.text)
-element.click()
-"""
-"""
-for nomuniversite in listenom:
+try:
+    while 1:
+        afficherPlus = driver.find_element(By.LINK_TEXT, "Afficher plus")
+        afficherPlus.location_once_scrolled_into_view
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, window.scrollY - 200)")
+        time.sleep(0.2)
+        afficherPlus.click()
+except:
     
-    print(lien.text)
-    lien.click()
-    driver.find_element(By.CLASS_NAME, "_modal_univ_moreclose").click()
-    
-    nomuniversite = nomuniversite.text
-    #Normalisation des noms d'université
-    idNom = nomuniversite.lower()
-    idnom = idNom.replace(" ","-")
-    idnomfinale = unidecode.unidecode(idnom)
-    print(nomuniversite)
-    print(idnomfinale)
-    if idnom not in listeEtablissement : 
-        etablissement = {}
-        etablissement["nom"]=nomuniversite
-        listeEtablissement[idnomfinale] = etablissement
-    
+    listeplusInfo = driver.find_elements(By.LINK_TEXT, "Plus de détails")
 
-    #a = driver.find_element(By.CLASS_NAME, "university_stats")
-"""
 
-print("coucou")
-driver.find_element(By.CLASS_NAME, "irm_filter_btn")
-print("coucou")
+    for plusInfo in listeplusInfo :
 
-with open('listeNom.json', 'w') as mon_fichier:
-    json.dump(listeEtablissement, mon_fichier)
+
+
+        time.sleep(1)
+        plusInfo.location_once_scrolled_into_view
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, window.scrollY - 200)")
+        
+
+
+        #//p[@class="_title" and text() = "Accord"]/../../../div[2]/div/p
+        time.sleep(1)
+        #Ouverture de la page "Plus d'infos"
+        plusInfo.click()
+        time.sleep(2)
+
+        nomuniversite = driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Etablissement externe"]/../../../div[2]/div/p').text
+        nomuniversite = unidecode.unidecode(nomuniversite)
+        idNom = nomuniversite.lower()   
+        idnom = idNom.replace(" ","-")
+        idnom = unidecode.unidecode(idnom)
+        
+
+        if idnom not in listeEtablissement : 
+            print("---------------------")
+            print(idNom)
+
+            etablissement = {}
+            
+
+            etablissement["nom"]=nomuniversite
+
+            pays = driver.find_element(By.XPATH, '//p[@class="_title" and text() = "PAYS (relation)"]/../../../div[2]/div/p')
+            print(pays.text)
+            etablissement["pays"]=unidecode.unidecode(pays.text)
+
+            Accord = []
+            accord = driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Accord"]/../../../div[2]/div/p')
+            print(accord.text)
+            Accord.append(unidecode.unidecode(accord.text))
+            etablissement["accord"]=Accord
+
+            place = driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Places - Nombre et durée"]/../../../div[2]/div/p')
+            print(place.text)
+            etablissement["place"]=unidecode.unidecode(place.text)
+
+            descr = driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Description"]/../../../div[2]/div/p')
+            print(descr.text)
+            etablissement["descr"]=unidecode.unidecode(descr.text)
+
+            niveauLV = driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Niveau de langues demandé"]/../../../div[2]/div/p')
+            print(niveauLV.text)
+            etablissement["niveauLV"]=unidecode.unidecode(niveauLV.text)
+
+            debuts1= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Début S1"]/../../../div[2]/div/p')
+            print(debuts1.text)
+            etablissement["debuts1"]=unidecode.unidecode(debuts1.text)
+
+            fins1= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Fin S1"]/../../../div[2]/div/p')
+            print(fins1.text)
+            etablissement["Fins1"]=unidecode.unidecode(fins1.text)
+
+            debuts2= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Début S2"]/../../../div[2]/div/p')
+            print(debuts2.text)
+            etablissement["debuts2"]=unidecode.unidecode(debuts2.text)
+
+            fins2= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Fin S2"]/../../../div[2]/div/p')
+            print(fins2.text)
+            etablissement["finS2"]=unidecode.unidecode(fins2.text)
+
+            candidature= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Mode de candidature"]/../../../div[2]/div/p')
+            print(candidature.text)
+            etablissement["candidature"]=unidecode.unidecode(candidature.text)
+            
+
+            #etablissement["pays"]=unidecode.unidecode(pays.text)
+            
+            
+            univBtn = driver.find_element(By.LINK_TEXT, "Etablissements")
+            univBtn.click()
+            time.sleep(1)
+
+            Ville= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Ville"]/../../../div[2]/div/p')
+            print(Ville.text)
+            etablissement["ville"]=unidecode.unidecode(Ville.text)
+
+            URL= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "URL"]/../../../div[2]/div/p')
+            print(URL.text)
+            etablissement["URL"]=unidecode.unidecode(URL.text)
+
+            Fichier= driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Fichier"]/../../../div[2]/div/p')
+            print(Fichier.text)
+            etablissement["URL"]=unidecode.unidecode(Fichier.text)
+
+
+
+
+            listeEtablissement[idnom] = etablissement
+
+        else:
+            accord = driver.find_element(By.XPATH, '//p[@class="_title" and text() = "Accord"]/../../../div[2]/div/p')
+            listeEtablissement[idnom]['accord'].append(unidecode.unidecode(accord.text))
+            print(accord.text)
+
+        #time.sleep(1)
+        #Ouverture de l'onglet "etablissement"
+
+        #TODO récup info général
+        time.sleep(1)
+        closeBtn = driver.find_element(By.XPATH, '//button[@class="_modal_univ_moreclose"]')
+        closeBtn.click()
+    #Fermeture de la page "Plus d'infos"
+
+
+
+    """
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Plus de détails")))
+    print(element.text)
+    element.click()
+    """
+    """
+    for nomuniversite in listenom:
+        
+        print(lien.text)
+        lien.click()
+        driver.find_element(By.CLASS_NAME, "_modal_univ_moreclose").click()
+        
+        nomuniversite = nomuniversite.text
+        #Normalisation des noms d'université
+        idNom = nomuniversite.lower()
+        idnom = idNom.replace(" ","-")
+        idnomfinale = unidecode.unidecode(idnom)
+        print(nomuniversite)
+        print(idnomfinale)
+        if idnom not in listeEtablissement : 
+            etablissement = {}
+            etablissement["nom"]=nomuniversite
+            listeEtablissement[idnomfinale] = etablissement
+        
+
+        #a = driver.find_element(By.CLASS_NAME, "university_stats")
+    """
+
+    with open('listedetaille.json', 'w') as mon_fichier:
+        json.dump(listeEtablissement, mon_fichier)
