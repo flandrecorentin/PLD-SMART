274c1fc5e91566eeb319319b31ab5eaf0d2eb02d
ThomasColin1 <saumontomate@gmail.com>
2023-05-09 11:33:07 +0200

    Début page home + page accueil

--- h4221-frontend/src/app/app-routing.module.ts
+++ h4221-frontend/src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { ConnexionPageComponent } from './connexion-page/connexion-page.componen
 import { FaqPageComponent } from './faq-page/faq-page.component';
 import { InscriptionPageComponent } from './inscription-page/inscription-page.component';
 import { FormPageComponent } from './form-page/form-page.component';
+import { AccueilPageComponent } from './accueil-page/accueil-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent},
+  { path: 'home', component: HomePageComponent},
   { path: 'connexion', component: ConnexionPageComponent},
   { path: 'inscription', component: InscriptionPageComponent},
   { path: 'faq', component: FaqPageComponent},
   { path: 'form', component: FormPageComponent},
+  { path: '', component: AccueilPageComponent},
   // { path: 'curriculum', component: CurriculumComponent},
   // { path: 'projects', component: ProjectsComponent},
   // { path: 'hobbies/sport', component: HobbiesSportComponent},
