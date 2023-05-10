c7cb42091c9d5191757c8c3c1738f57b71835290
Flandre Corenitn <flandrecorentin@gmail.com>
2023-05-10 08:57:28 +0200

    Initialisation des FAQ et de l'utilisateur Tom

--- h4221-backend/src/main/java/ifinsa/h4221backend/Initialisation.java
+++ h4221-backend/src/main/java/ifinsa/h4221backend/Initialisation.java
@@ -1,8 +1,10 @@
 package ifinsa.h4221backend;
 
 import ifinsa.h4221backend.model.Departement;
+import ifinsa.h4221backend.model.FAQ;
 import ifinsa.h4221backend.model.Universite;
 import ifinsa.h4221backend.model.User;
+import ifinsa.h4221backend.service.FAQService;
 import ifinsa.h4221backend.service.UniversiteService;
 import ifinsa.h4221backend.service.UserService;
 import org.json.simple.JSONObject;
@@ -26,6 +28,9 @@ public class Initialisation implements ApplicationRunner {
     @Autowired
     UniversiteService universiteService;
 
+    @Autowired
+    FAQService faqService;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // appel des services à faire à l'initialisation
@@ -33,14 +38,16 @@ public class Initialisation implements ApplicationRunner {
 
         // Initialisation des "Users"
         List<User> users= new LinkedList<>();
-        User corentin = new User("corentin.flandre@insa-lyon.fr", "Flandre", "Corentin", Departement.IF, 4, "Kungliga Tekniska högskolan", "Suède", "123456");
-        User colin = new User("colin.thomas@insa-lyon.fr", "Thomas", "Colin", Departement.IF, 4, "Norges teknisk-naturvitenskapelige universitet", "Norvège", "123456");
-        User elise = new User("elise.dubillot@insa-lyon.fr", "Dubillot", "Elise", Departement.IF, 4, "Trinity College", "Irlande", "123456");
+        User corentin = new User("corentin.flandre@insa-lyon.fr", "Flandre", "Corentin", Departement.IF, 4, "kungliga-tekniska-hogskolan-(kth)", "Suede", "123456");
+        User colin = new User("colin.thomas@insa-lyon.fr", "Thomas", "Colin", Departement.IF, 4, "norges-teknisk-naturvitenskapelige-universitet", "Norvege", "123456");
+        User elise = new User("elise.dubillot@insa-lyon.fr", "Dubillot", "Elise", Departement.IF, 4, "university-college-dublin", "Irlande", "123456");
+        User tom = new User("tom.delaporte@insa-lyon.fr", "Delaporte", "Tom", Departement.IF, 5, "university-of-birmingham", "Royaume-Uni", "123456");
         User elod = new User("admin@insa-lyon.fr", "Elod", "Admin", Departement.IF, 0, null, null, "12345678");
         elod.setRole("ROLE_ADMIN");
         users.add(corentin);
         users.add(colin);
         users.add(elise);
+        users.add(tom);
         users.add(elod);
         for (User user: users) {
             userService.inscrireService(user);
@@ -61,6 +68,25 @@ public class Initialisation implements ApplicationRunner {
         }
         for (Universite universite: universites) {
             universiteService.sauvegarderUniversite(universite);
+        }
+
+
+        // Initialisation des formulairesREX
+
+
+
+        // Initialisation FAQ
+        List<FAQ> faqs = new LinkedList<>();
+        FAQ faq1 = new FAQ("Quels sont les documents à remplir pour la bourse Erasmus+?","Bourses", "Vous recevrez un mail vous demandant de vous connecter à MoveOn pour remplir votre OLA (Online Learning Agreement). Vous devrez en plus remplir un contrat d'études auprès de votre département.","colin.thomas@insa-lyon.fr", "elod@insa-lyon.fr","07-04-2023", "10-05-2023");
+        FAQ faq2 = new FAQ("Combien de voeux peut-on demander ?","Voeux", "Cela dépend du département. Par exemple, en IF 2 voeux sont initialement demandés. Contactez votre coordinateur de département pour connaitre ce nombre.","elise.dubillot@insa-lyon.fr", "elod@insa-lyon.fr","09-05-2023", "10-05-2023");
+        FAQ faq3 = new FAQ("Existe-t-il des conditions spécifiques pour demander un double diplôme (DD) ?","Candidature", "Il n y a aucunes conditions demandés par l'INSA pour les DD","corentin.flandre@insa-lyon.fr", "elod@insa-lyon.fr","05-05-2023", "08-05-2023");
+        FAQ faq4 = new FAQ("Qu'est-ce qu'est la bourse AMI","Bourses", "C'est une bourse spécifique pour les étudiants boursiers du Crous","colin.thomas@insa-lyon.fr", "elod@insa-lyon.fr","05-05-2023", "10-05-2023");
+        faqs.add(faq1);
+        faqs.add(faq2);
+        faqs.add(faq3);
+        faqs.add(faq4);
+        for (FAQ faq: faqs) {
+            faqService.createFAQ(faq);
         }
 
         System.out.println("|| Fin Initialisation ");
