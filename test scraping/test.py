6a209197f32b308e1e3d4b6f58f3ebea0e45b363
dorian-brd <bernard.dorian1@gmail.com>
2023-05-11 08:23:09 +0200

    petit clean rapide

--- test scraping/test.py
+++ test scraping/test.py
@@ -38,33 +38,7 @@ btnRecherche2 = driver.find_element(By.XPATH, '//button[@class="irm_filter_btn m
 btnRecherche2.click()
 time.sleep(1)
 """
-"""
-#Scroll down: Il faut laisser le temps au driver de charger la page avant d'ouvrir "plus de détails"
-time.sleep(2)
-print(test.text)
-driver.execute_script("arguments[0].scrollIntoView();",test)
-test.click
-driver.execute_script("window.scrollTo(0, 700,)")
-"""
 
-"""
-test = driver.find_element(By.LINK_TEXT, "Plus de détails")
-time.sleep(1)
-print(test.text)
-driver.execute_script("arguments[0].scrollIntoView();",test)
-time.sleep(1)
-test.click()
-"""
-
-"""
-#Fonctionne bien pour scroller d'une université à l'autre.
-test = driver.find_elements(By.LINK_TEXT, "Plus de détails")
-for t in test :
-    time.sleep(1)
-    print(t.text)
-    driver.execute_script("arguments[0].scrollIntoView();",t)
-time.sleep(2)
-"""
 
 #Scrollig parfait pour ouvrir la première page "Plus d'infos"
 try:
@@ -287,37 +261,6 @@ try:
             closeBtn.click()
             #json.dump(listeEtablissement, nom_fichier)
         #Fermeture de la page "Plus d'infos"
-
-
-
-        """
-        wait = WebDriverWait(driver, 10)
-        element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Plus de détails")))
-        print(element.text)
-        element.click()
-        """
-        """
-        for nomuniversite in listenom:
-
-        print(lien.text)
-        lien.click()
-        driver.find_element(By.CLASS_NAME, "_modal_univ_moreclose").click()
-
-        nomuniversite = nomuniversite.text
-        #Normalisation des noms d'université
-        idNom = nomuniversite.lower()
-        idnom = idNom.replace(" ","-")
-        idnomfinale = unidecode.unidecode(idnom)
-        print(nomuniversite)
-        print(idnomfinale)
-        if idnom not in listeEtablissement : 
-            etablissement = {}
-            etablissement["nom"]=nomuniversite
-            listeEtablissement[idnomfinale] = etablissement
-
-
-        #a = driver.find_element(By.CLASS_NAME, "university_stats")
-        """
 
         with open('listedetaillefinales.json', 'w') as mon_fichier:
             json.dump(listeEtablissement, mon_fichier)
