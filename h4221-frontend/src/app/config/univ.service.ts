4b7472b0a387906eb80c1ef65f51962bb0ec5237
ThomasColin1 <saumontomate@gmail.com>
2023-05-11 11:44:38 +0200

    Correction pas de message + boutons grisés faq

--- h4221-frontend/src/app/config/univ.service.ts
+++ h4221-frontend/src/app/config/univ.service.ts
@@ -9,6 +9,7 @@ export class UnivService {
   // Links to call back-end services
   backendUrl = 'http://localhost:9000'
   univURL = this.backendUrl + '/universite';
+  univDetailsURL = this.backendUrl+'/universite/details/';
   httpHeaders = new HttpHeaders({
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem("auth_token")}`,
@@ -26,5 +27,9 @@ export class UnivService {
   // http://localhost:9000/universite/Suede
   getAllUnivsByCountry(form:any) {
     return this.http.get<JSON>(this.univURL+"/"+form, {headers: this.httpHeaders});
+  }
+
+  getDetailsUniv(univId:string) {
+    return this.http.get<JSON>(this.univDetailsURL+univId, {headers: this.httpHeaders});
   }
 }
